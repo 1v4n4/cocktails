7e790d868ac16101f5374179b262a098331a1c4f
ja <ivananl@yahoo.com>
2021-09-02 12:53:10 +0200

    Mobile layout fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,7 @@ const Header = () => (
     <Link to="/"><div className="logo-div"><img className="logo" src={logo1} alt="logo" /></div></Link>
     <div className="right">
       <Link className="nav-link" to="/alcoholic">ALCOHOLIC</Link>
-      <br className="new-line" />
       <Link className="nav-link" to="/non-alcoholic">NON-ALCOHOLIC</Link>
-      <br className="new-line1" />
-
       <Link className="nav-link" to="/ingredients">SELECT BY INGREDIENT</Link>
     </div>
 
